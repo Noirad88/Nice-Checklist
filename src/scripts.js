197511eccbdae8e896772639a313740945f83542
Darion <noirad@gmail.com>
2025-11-09 02:50:56 -0500

    using crypto

--- src/scripts.js
+++ src/scripts.js
@@ -1,110 +1,99 @@
-let v4
+class Note{
+    constructor(){
+        this.id = 'note-' + crypto.randomUUID()
+        this.text = ""
+        this.done = false
+        this.archived = false
+    }
 
-( async() =>{
-    try{
-        const module = await import('../node_modules/uuid/dist/v4.js');
-        v4 = module.default;
+    handleOnDelete(){
+        this.archived = !this.archived
+    }
 
-        class Note{
-            constructor(){
-                this.id = 'note-' + v4()
-                this.text = ""
-                this.done = false
-                this.archived = false
-            }
+    handleOnDone(e){
+        this.done = e.target.checked
+    }
 
-            handleOnDelete(){
-                this.archived = !this.archived
-            }
+    handleOnTextChange(e){
+        this.text = e.target.value
+    }
 
-            handleOnDone(e){
-                this.done = e.target.checked
-            }
+    getHTML(){
+        return `
+                <div id=${this.id} class='note-item ${this.archived == true ? 'is-archived' : ''} ${this.done == true ? 'is-done' : ''}'>
+                    <input title='done' name='done' type='checkbox' value='done' class='note-done' ${this.done && 'checked'}/>
+                    <input title='note text' name='note text' class='note-text' type='text' value='${this.text}'>
+                    <button title='delete note' name='delete note' class='note-button note-delete'>${this.archived ? 'Un-hide' : 'Hide'}</button>
+                </div>
+            `
+    }
 
-            handleOnTextChange(e){
-                this.text = e.target.value
-            }
+}
 
-            getHTML(){
-                return `
-                        <div id=${this.id} class='note-item ${this.archived == true ? 'is-archived' : ''} ${this.done == true ? 'is-done' : ''}'>
-                            <input title='done' name='done' type='checkbox' value='done' class='note-done' ${this.done && 'checked'}/>
-                            <input title='note text' name='note text' class='note-text' type='text' value='${this.text}'>
-                            <button title='delete note' name='delete note' class='note-button note-delete'>${this.archived ? 'Un-hide' : 'Hide'}</button>
-                        </div>
-                    `
-            }
+class NotesManager{
 
-        }
+    constructor(){
+        this._notes = []
+        this.notesContainer = document.querySelector('#notes-container')
+        this.showArchived = false
+    }
 
-        class NotesManager{
+    addNote(){
+        const newNote = new Note
+        this._notes.push(newNote)
+        this.render()
+    }
 
-            constructor(){
-                this._notes = []
-                this.notesContainer = document.querySelector('#notes-container')
-                this.showArchived = false
-            }
+    render(){
 
-            addNote(){
-                const newNote = new Note
-                this._notes.push(newNote)
+        this.notesContainer = document.querySelector('#notes-container')
+        this.notesContainer.innerHTML = ''
+
+        const activeNotes = this._notes.filter((note) => {
+            return (this.showArchived == true) || (this.showArchived == false && note.archived == false)
+        })
+
+        activeNotes.map((note)=>{
+
+            const noteElement = note.getHTML()
+
+            this.notesContainer.insertAdjacentHTML('beforeend',noteElement)
+
+            const noteCheck = this.notesContainer.querySelector(`#${note.id} input.note-done`)
+            noteCheck.addEventListener('change',(e)=>{
+                note.handleOnDone(e)
                 this.render()
-            }
 
-            render(){
+            })
 
-                this.notesContainer = document.querySelector('#notes-container')
-                this.notesContainer.innerHTML = ''
+            const noteClose = this.notesContainer.querySelector(`#${note.id} button.note-delete`)
+            noteClose.addEventListener('click',()=>{
+                note.handleOnDelete()
+                this.render()
+            })
 
-                const activeNotes = this._notes.filter((note) => {
-                    return (this.showArchived == true) || (this.showArchived == false && note.archived == false)
-                })
+            const noteText = this.notesContainer.querySelector(`#${note.id} input.note-text`)
+            noteText.addEventListener('input',(e)=>{
+                note.handleOnTextChange(e)
+            })
 
-                activeNotes.map((note)=>{
-
-                    const noteElement = note.getHTML()
-
-                    this.notesContainer.insertAdjacentHTML('beforeend',noteElement)
-
-                    const noteCheck = this.notesContainer.querySelector(`#${note.id} input.note-done`)
-                    noteCheck.addEventListener('change',(e)=>{
-                        note.handleOnDone(e)
-                        this.render()
-
-                    })
-
-                    const noteClose = this.notesContainer.querySelector(`#${note.id} button.note-delete`)
-                    noteClose.addEventListener('click',()=>{
-                        note.handleOnDelete()
-                        this.render()
-                    })
-
-                    const noteText = this.notesContainer.querySelector(`#${note.id} input.note-text`)
-                    noteText.addEventListener('input',(e)=>{
-                        note.handleOnTextChange(e)
-                    })
-
-                })
-            }
-        }
-
-        const noteList = new NotesManager()
-
-        document.querySelector('#nav-add-note').addEventListener('click',()=>{
-            noteList.addNote()
-        })
-
-        document.addEventListener('DOMContentLoaded',()=>{
-            noteList.addNote()
-        })
-
-        document.querySelector('#nav-show-complete').addEventListener('change',(e)=>{
-            const showCompleteState = e.target.checked
-            noteList.showArchived = showCompleteState
-            noteList.render()
         })
     }
-    catch(e){
-        console.error(e)
-    }
-})()
+}
+
+const noteList = new NotesManager()
+
+document.querySelector('#nav-add-note').addEventListener('click',()=>{
+    noteList.addNote()
+})
+
+document.addEventListener('DOMContentLoaded',()=>{
+    noteList.addNote()
+})
+
+document.querySelector('#nav-show-complete').addEventListener('change',(e)=>{
+    const showCompleteState = e.target.checked
+    noteList.showArchived = showCompleteState
+    noteList.render()
+})
+    
